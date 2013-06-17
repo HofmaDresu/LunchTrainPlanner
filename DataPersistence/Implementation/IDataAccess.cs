093ff7e90de50cd1ede536febeb6b2ff72710e15
HofmaDresu <hofmadresu@gmail.com>
2013-06-17 07:35:51 -0400

    Add data persistence project

---
+++ DataPersistence/Implementation/IDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPersistence.Implementation
+{
+    public interface IDataAccess
+    {
+        void StoreSuggestion(string suggestion);
+        int ClearSuggestionSession();
+        void removeSuggestion(string suggestion);
+        string GetSuggestions();
+    }
+}
